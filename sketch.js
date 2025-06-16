a820ef8e695b2a083fda8fb993ef802019d29e25
Alka2222 <adobosz4@st.swps.edu.pl>
2025-06-16 20:26:43 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,10 +6,10 @@ let loremWords = [
 
 let words = [];
 let fontSize = 32;
-let minFontSize = 20;
+let minFontSize = 13;
 let finalFontSize = null;
 let typedLetters = 0;
-let avoidRadius = 500;
+let avoidRadius = 100;
 
 let endTextX = 0;
 let endTextY = 0;
